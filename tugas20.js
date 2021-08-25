c2bcb9c7f9b0fd34562b31037236b1ca585d2eed
lahiardhan <lahia.ardhanm@gmail.com>
2021-08-25 17:00:29 +0700

    Tugas 20 JS
    Tugas ini saya serahkan kepada Tim Niomic

---
+++ tugas20.js
@@ -0,0 +1,4 @@
+const kalimat =  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+console.log(kalimat.match(/are/));
+console.log(null);
+console.log(kalimat.match(/words/));
